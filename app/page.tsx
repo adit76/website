01bdc0f03787130e9f876a25714f6a14fcada824
adit76 <aditsapkota.as@gmail.com>
2025-11-26 17:02:39 +1030

    Home Page >> Header + Banner Section Created

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,82 @@
 import Image from "next/image";
+import { Container, Stack, Button } from "@mui/material";
+import MeteorShower from "./components/meteorShower/page";
+
+export function HeroBanner() {
+  return (
+    <MeteorShower className="flex aspect-16/9 items-end justify-center">
+      <Container>
+        <div className="banner-content min-h-screen text-center pt-35">
+          <div className="banner-items">
+            <h1 className="text-4xl font-bold mb-4">
+              Welcome to Ninja IT Solutions
+            </h1>
+            <p className="text-lg mb-8">
+              Your one-stop solution for all your hospitality needs.
+            </p>
+            <Button variant="contained" size="large">
+              Get Started
+            </Button>
+          </div>
+          <div className="hero-img pt-20">
+            <Image
+              src="/hero-image.png"
+              alt="Hero Image"
+              width={1800}
+              height={0}
+              priority
+            />
+          </div>
+        </div>
+      </Container>
+    </MeteorShower>
+  );
+}
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+    <main>
+      <header>
+        <Container>
+          <div className="header-main flex p-4 items-center justify-between">
+            <div className="logo">
+              <Image
+                src="/logo.jpg"
+                alt="Ninja IT"
+                width={200}
+                height={0}
+                priority
+              />
+            </div>
+            <div className="menu-items">
+              <ul className="flex gap-8">
+                <li>Home</li>
+                <li>Services</li>
+                <li>About Us</li>
+                <li>Contact Us</li>
+              </ul>
+            </div>
+            <div className="actions">
+              <Stack spacing={2} direction="row">
+                <Button variant="text">Login</Button>
+                <Button variant="contained">Contact Us</Button>
+              </Stack>
+            </div>
+          </div>
+        </Container>
+      </header>
+      <section id="main-banner">
+        <HeroBanner />
+      </section>
+      <section id="services" style={{ background: "#F7F7F7" }}>
+        <Container>
+          <div className="services-section py-20">
+            <h2 className="text-3xl font-bold mb-10 text-black">
+              Our Services
+            </h2>
+          </div>
+        </Container>
+      </section>
+    </main>
   );
 }
