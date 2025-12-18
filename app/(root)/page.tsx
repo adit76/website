75ad684311d3a3d2c1ee47b92d4da75b9123daa9
adit76 <aditsapkota.as@gmail.com>
2025-12-18 11:24:18 +1030

    Footer Design Completed

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,21 @@
 import Image from "next/image";
 import { Container, Button, Typography, Grid, Divider } from "@mui/material";
-import CheckIcon from "@mui/icons-material/Check";
+import {
+  Check,
+  Facebook,
+  LinkedIn,
+  X,
+  Instagram,
+  Send,
+} from "@mui/icons-material";
 import MeteorShower from "../components/meteorShower/page";
 import Marquee from "react-fast-marquee";
+import Paper from "@mui/material/Paper";
+import InputBase from "@mui/material/InputBase";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
+import DirectionsIcon from "@mui/icons-material/Directions";
 
 export function HeroBanner() {
   return (
@@ -17,7 +30,9 @@ export function HeroBanner() {
               {" "}
               Your one-stop solution for all your hospitality needs.
             </Typography>
-            <Button variant="contained">Get Started</Button>
+            <Button variant="contained" sx={{ fontWeight: "bold" }}>
+              Get Started
+            </Button>
           </div>
           <div className="hero-img pt-20">
             <Image
@@ -94,9 +109,7 @@ export default function Home() {
                   Streamline your operations with our cutting-edge POS systems
                   designed for the hospitality industry.
                 </Typography>
-                <Button variant="text" sx={{ fontWeight: "bold" }}>
-                  Learn More
-                </Button>
+                <Button variant="text">Learn More</Button>
               </div>
             </Grid>
 
@@ -295,6 +308,8 @@ export default function Home() {
           </Grid>
         </Container>
       </section>
+
+      {/* Pricing */}
       <section
         id="pricing"
         className="px-8 py-16"
@@ -377,7 +392,7 @@ export default function Home() {
                         className="number-box w-8 h-8 rounded-full text-white font-bold flex items-center justify-center "
                         style={{ background: "#CC1D2E" }}
                       >
-                        <CheckIcon />
+                        <Check />
                       </div>
                       <Typography
                         variant="subtitle1"
@@ -391,7 +406,7 @@ export default function Home() {
                         className="number-box w-8 h-8 rounded-full text-white font-bold flex items-center justify-center "
                         style={{ background: "#CC1D2E" }}
                       >
-                        <CheckIcon />
+                        <Check />
                       </div>
                       <Typography
                         variant="subtitle1"
@@ -405,7 +420,7 @@ export default function Home() {
                         className="number-box w-8 h-8 rounded-full text-white font-bold flex items-center justify-center "
                         style={{ background: "#CC1D2E" }}
                       >
-                        <CheckIcon />
+                        <Check />
                       </div>
                       <Typography
                         variant="subtitle1"
@@ -419,7 +434,7 @@ export default function Home() {
                         className="number-box w-8 h-8 rounded-full text-white font-bold flex items-center justify-center "
                         style={{ background: "#CC1D2E" }}
                       >
-                        <CheckIcon />
+                        <Check />
                       </div>
                       <Typography
                         variant="subtitle1"
@@ -433,7 +448,7 @@ export default function Home() {
                         className="number-box w-8 h-8 rounded-full text-white font-bold flex items-center justify-center "
                         style={{ background: "#CC1D2E" }}
                       >
-                        <CheckIcon />
+                        <Check />
                       </div>
                       <Typography
                         variant="subtitle1"
@@ -447,7 +462,7 @@ export default function Home() {
                         className="number-box w-8 h-8 rounded-full text-white font-bold flex items-center justify-center "
                         style={{ background: "#CC1D2E" }}
                       >
-                        <CheckIcon />
+                        <Check />
                       </div>
                       <Typography
                         variant="subtitle1"
@@ -521,7 +536,7 @@ export default function Home() {
                         className="number-box w-8 h-8 rounded-full text-white font-bold flex items-center justify-center "
                         style={{ background: "#CC1D2E" }}
                       >
-                        <CheckIcon />
+                        <Check />
                       </div>
                       <Typography
                         variant="subtitle1"
@@ -535,7 +550,7 @@ export default function Home() {
                         className="number-box w-8 h-8 rounded-full text-white font-bold flex items-center justify-center "
                         style={{ background: "#CC1D2E" }}
                       >
-                        <CheckIcon />
+                        <Check />
                       </div>
                       <Typography
                         variant="subtitle1"
@@ -549,7 +564,7 @@ export default function Home() {
                         className="number-box w-8 h-8 rounded-full text-white font-bold flex items-center justify-center "
                         style={{ background: "#CC1D2E" }}
                       >
-                        <CheckIcon />
+                        <Check />
                       </div>
                       <Typography
                         variant="subtitle1"
@@ -563,7 +578,7 @@ export default function Home() {
                         className="number-box w-8 h-8 rounded-full text-white font-bold flex items-center justify-center "
                         style={{ background: "#CC1D2E" }}
                       >
-                        <CheckIcon />
+                        <Check />
                       </div>
                       <Typography
                         variant="subtitle1"
@@ -577,7 +592,7 @@ export default function Home() {
                         className="number-box w-8 h-8 rounded-full text-white font-bold flex items-center justify-center "
                         style={{ background: "#CC1D2E" }}
                       >
-                        <CheckIcon />
+                        <Check />
                       </div>
                       <Typography
                         variant="subtitle1"
@@ -591,7 +606,7 @@ export default function Home() {
                         className="number-box w-8 h-8 rounded-full text-white font-bold flex items-center justify-center "
                         style={{ background: "#CC1D2E" }}
                       >
-                        <CheckIcon />
+                        <Check />
                       </div>
                       <Typography
                         variant="subtitle1"
@@ -665,7 +680,7 @@ export default function Home() {
                         className="number-box w-8 h-8 rounded-full text-white font-bold flex items-center justify-center "
                         style={{ background: "#CC1D2E" }}
                       >
-                        <CheckIcon />
+                        <Check />
                       </div>
                       <Typography
                         variant="subtitle1"
@@ -679,7 +694,7 @@ export default function Home() {
                         className="number-box w-8 h-8 rounded-full text-white font-bold flex items-center justify-center "
                         style={{ background: "#CC1D2E" }}
                       >
-                        <CheckIcon />
+                        <Check />
                       </div>
                       <Typography
                         variant="subtitle1"
@@ -693,7 +708,7 @@ export default function Home() {
                         className="number-box w-8 h-8 rounded-full text-white font-bold flex items-center justify-center "
                         style={{ background: "#CC1D2E" }}
                       >
-                        <CheckIcon />
+                        <Check />
                       </div>
                       <Typography
                         variant="subtitle1"
@@ -707,7 +722,7 @@ export default function Home() {
                         className="number-box w-8 h-8 rounded-full text-white font-bold flex items-center justify-center "
                         style={{ background: "#CC1D2E" }}
                       >
-                        <CheckIcon />
+                        <Check />
                       </div>
                       <Typography
                         variant="subtitle1"
@@ -721,7 +736,7 @@ export default function Home() {
                         className="number-box w-8 h-8 rounded-full text-white font-bold flex items-center justify-center "
                         style={{ background: "#CC1D2E" }}
                       >
-                        <CheckIcon />
+                        <Check />
                       </div>
                       <Typography
                         variant="subtitle1"
@@ -735,7 +750,7 @@ export default function Home() {
                         className="number-box w-8 h-8 rounded-full text-white font-bold flex items-center justify-center "
                         style={{ background: "#CC1D2E" }}
                       >
-                        <CheckIcon />
+                        <Check />
                       </div>
                       <Typography
                         variant="subtitle1"
@@ -759,6 +774,8 @@ export default function Home() {
           </Grid>
         </Container>
       </section>
+
+      {/* Our Clients */}
       <section
         id="clients"
         className="px-8 py-16"
@@ -839,6 +856,8 @@ export default function Home() {
           </Grid>
         </Container>
       </section>
+
+      {/* Call to Action */}
       <section
         id="cta"
         className="px-8 py-16"
@@ -864,7 +883,7 @@ export default function Home() {
               className="absolute top-0 right-0"
             />
 
-            <div className="services-section mb-16 text-center w-full">
+            <div className="services-section mb-12 text-center w-full">
               <Typography
                 variant="h3"
                 sx={{ fontWeight: "bold", color: "white" }}
@@ -873,11 +892,7 @@ export default function Home() {
                 Upgrade Your Business with Our <br />
                 Cutting-Edge POS Solutions!
               </Typography>
-              <Typography
-                sx={{ color: "#F7F7F7" }}
-                variant="subtitle1"
-                gutterBottom
-              >
+              <Typography sx={{ color: "#F7F7F7" }} variant="h6" gutterBottom>
                 Experience the future of transactions! Schedule a demo today and{" "}
                 <br />
                 witness how our POS solution can revolutionize your business.
@@ -906,6 +921,76 @@ export default function Home() {
           </div>
         </Container>
       </section>
+
+      <footer className="p-4" style={{ background: "#000" }}>
+        <Container>
+          <Grid container spacing={2} className="footer-wrapper my-16">
+            <Grid size={4}>
+              <Image src="/logo.jpg" width={180} height={100} alt="Logo" />
+              <Typography variant="body1" sx={{ color: "#E0E0E0", mt: 4 }}>
+                Ninja IT provide a Complete solution for all Hospitality POS &
+                IT support needs in Adelaide. We specialise in Cafes,
+                Restaurants and Retail environments and their unique
+                requirements for POS systems.
+              </Typography>
+              <div className="socials flex mt-8 gap-4">
+                <Facebook sx={{ color: "#E0E0E0", cursor: "pointer" }} />
+                <X sx={{ color: "#E0E0E0", cursor: "pointer" }} />
+                <LinkedIn sx={{ color: "#E0E0E0", cursor: "pointer" }} />
+                <Instagram sx={{ color: "#E0E0E0", cursor: "pointer" }} />
+              </div>
+            </Grid>
+            <Grid size={2} offset={1}>
+              <Typography variant="h5">Quick Links</Typography>
+              <ul className="mt-8 space-y-3" style={{ color: "#E0E0E0" }}>
+                <li>Home</li>
+                <li>Services</li>
+                <li>About Us</li>
+                <li>Contact Us</li>
+              </ul>
+            </Grid>
+            <Grid size={2}>
+              <Typography variant="h5">Support</Typography>
+              <ul className="mt-8 space-y-3" style={{ color: "#E0E0E0" }}>
+                <li>FAQ&apos;s</li>
+                <li>Articles</li>
+                <li>Live Chat</li>
+              </ul>
+            </Grid>
+            <Grid size={3}>
+              <Typography variant="h5">Subscribe Newsletter</Typography>
+              <Paper
+                component="form"
+                sx={{
+                  p: "2px 4px",
+                  display: "flex",
+                  alignItems: "center",
+                  width: "100%",
+                  mt: 4,
+                }}
+              >
+                <InputBase
+                  sx={{ ml: 1, flex: 1 }}
+                  placeholder="Your Email"
+                  inputProps={{ "aria-label": "search google maps" }}
+                />
+
+                <Button variant="contained" endIcon={<Send />}>
+                  Send
+                </Button>
+              </Paper>
+            </Grid>
+          </Grid>
+
+          <Divider sx={{ borderColor: "rgba(117,117,117,0.5)", my: 2 }} />
+          <Typography
+            variant="body1"
+            sx={{ color: "#757575", textAlign: "center" }}
+          >
+            © Copyright 2025. Design by Adit
+          </Typography>
+        </Container>
+      </footer>
     </main>
   );
 }
